3ef03fe9f96b643bbc46d4fc63adbccb49da7f31
Pablo Teles <88568500+pablotelles@users.noreply.github.com>
2022-04-25 15:16:16 -0300

    finished project
    finished project

--- src/components/Menu/Pizza/Tags/TagOnCart.jsx
+++ src/components/Menu/Pizza/Tags/TagOnCart.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import './style.scss'
 import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
 
-export const TagOnCart = ({ cart, product }) => {
+const TagOnCart = ({ cart, product }) => {
   const validationProduct = cart.some(item => item.id === product.id) === true
   if (validationProduct) {
     return (
@@ -15,5 +16,11 @@ export const TagOnCart = ({ cart, product }) => {
 
 TagOnCart.propTypes = {
   product: PropTypes.object.isRequired,
-  cart: PropTypes.array.isRequired
+  cart: PropTypes.array
 }
+const mapStateToProps = state => {
+  return {
+    cart: state.cart
+  }
+}
+export default connect(mapStateToProps)(TagOnCart)
