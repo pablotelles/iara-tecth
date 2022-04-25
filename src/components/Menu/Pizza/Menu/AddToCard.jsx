e0b637e3b991b0f3c5efae22be798ef593636bcb
Pablo Teles <88568500+pablotelles@users.noreply.github.com>
2022-04-25 13:45:41 -0300

    new-feature
    Remove from cart on initial page - ok
    Promotion price - ok

--- src/components/Menu/Pizza/Menu/AddToCard.jsx
+++ src/components/Menu/Pizza/Menu/AddToCard.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import './style.scss'
 import PropTypes from 'prop-types'
-import { handlerAddProduct } from '../../../../pages/Cart/handlerCart'
+import { handlerAddProduct, handlerRemoveProduct } from '../../../../pages/Cart/handlerCart'
 import { GrAdd as AddCartIcon } from 'react-icons/gr'
-import { BsCheckLg as CartCheck } from 'react-icons/bs'
+import { BsCheckLg as CartCheck, BsTrash as RemoveCartIcon } from 'react-icons/bs'
 
 export const AddToCard = ({ cart, product }) => {
   const validationProduct = cart.some(item => item.id === product.id) === true
@@ -11,9 +11,18 @@ export const AddToCard = ({ cart, product }) => {
 
   return (
     cart.some(item => item.id === product.id) === true
-      ? (<CartCheck
-                  className='menu__item--addCart menu__item--cartCheck'
-                  />)
+      ? (
+        <>
+          <CartCheck
+            className='menu__item--addCart menu__item--cartCheck'
+          />
+          <RemoveCartIcon
+            className='menu__item--remove'
+            onClick={() => handlerRemoveProduct(product)}
+          />
+        </>
+        )
+
       : <AddCartIcon
                   className='menu__item--addCart'
                   onClick={() => {
---
+++ src/components/Menu/Pizza/Menu/ProductPrice.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { AddToCard } from './AddToCard'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+
+export const ProductPrice = ({ product, cart }) => {
+  const validationPromotion = product.promotionDay
+  const promotionDiscount = product.price * 0.20
+
+  return (
+        <div className='menu__item--content'>
+            <p className={validationPromotion
+              ? 'menu__item--price menu__item--old-price'
+              : 'menu__item--price'}>{product.price}</p>
+            <p className={validationPromotion
+              ? 'menu__item--price menu__item__promotion--price-show'
+              : 'menu__item--price menu__item__promotion--price-hide'}>{product.promotionDay ? product.price - promotionDiscount : ''}</p>
+            <AddToCard cart={cart} product={product} />
+        </div>
+  )
+}
+ProductPrice.propTypes = {
+  product: PropTypes.object,
+  cart: PropTypes.array
+}
+
+const mapStateToProps = state => {
+  return {
+    products: state.product.pizzasData,
+    cart: state.cart
+  }
+}
+export default connect(mapStateToProps)(ProductPrice)
